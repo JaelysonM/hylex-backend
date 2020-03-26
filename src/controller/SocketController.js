9613362cb27fcb83be8dd474b193f9d4d016d9fb
Jaelyson Martins <jaelysonmartins@gmail.com>
2020-03-26 19:38:07 -0300

    Removido um debug

--- src/controller/SocketController.js
+++ src/controller/SocketController.js
@@ -16,16 +16,6 @@ const run = () => {
         storage[socket.id] = {serverName: server};
         server_id[server] = {serverId: socket.id};
         console.log(`\n\x1b[32m⇅ \x1b[43m\x1b[30m backend - socket.io \x1b[0m A new connection has been opened, id: \x1b[1m${socket.id} - Client name:${server}\x1b[0m`); 
-        setTimeout(() => {
-          if (server == "discord") {
-            io.to(`${server_id["discord"].serverId}`).emit("loginstaff", {
-              account_id: "280474473856237569",
-              nickname: "JaelysonMarth", 
-              activation_token: "22fg4#"
-        
-            });
-          }
-        }, 6*2000);
       }else {
         console.log(`\n\x1b[31m✖ \x1b[43m\x1b[30m backend - socket.io \x1b[0m A new connection has been force closed because we already have a client with name: \x1b[1m${server}\x1b[0m`); 
         socket.disconnect();
